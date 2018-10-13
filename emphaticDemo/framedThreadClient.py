a873104e5ec681f5f12b536b8e0dc8fc374f16cc
SushiRoll53 <brfigueroa@miners.utep.edu>
2018-10-12 18:36:00 -0600

    Added a mutex lock so it waits for a file to finish writing

--- emphaticDemo/framedThreadClient.py
+++ emphaticDemo/framedThreadClient.py
@@ -1,16 +1,18 @@
 #! /usr/bin/env python3
 
 # Echo client program
-import socket, sys, re
+import socket, sys, re, os
 import params
 from framedSock import FramedStreamSock
 from threading import Thread
+import threading
 import time
 
 switchesVarDefaults = (
     (('-s', '--server'), 'server', "localhost:50001"),
     (('-d', '--debug'), "debug", False), # boolean (set if present)
     (('-?', '--usage'), "usage", False), # boolean (set if present)
+    (('-p', '--put'), "put", "default"),
     )
 
 
@@ -18,6 +20,8 @@ progname = "framedClient"
 paramMap = params.parseParams(switchesVarDefaults)
 
 server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+fileName = paramMap["put"]
 
 if usage:
     params.usage()
@@ -30,12 +34,14 @@ except:
     print("Can't parse server:port from '%s'" % server)
     sys.exit(1)
 
-class ClientThread(Thread):
+class ClientThread(Thread):	
     def __init__(self, serverHost, serverPort, debug):
+        global fileName
         Thread.__init__(self, daemon=False)
         self.serverHost, self.serverPort, self.debug = serverHost, serverPort, debug
         self.start()
     def run(self):
+       global fileName
        s = None
        for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
            af, socktype, proto, canonname, sa = res
@@ -63,12 +69,31 @@ class ClientThread(Thread):
        fs = FramedStreamSock(s, debug=debug)
 
 
-       print("sending hello world")
-       fs.sendmsg(b"hello world")
-       print("received:", fs.receivemsg())
+       #print("sending hello world")
+       #fs.sendmsg(b"hello world")
+       #print("received:", fs.receivemsg())
 
-       fs.sendmsg(b"hello world")
-       print("received:", fs.receivemsg())
+       if os.path.isfile(fileName):
+       	print(fileName)
+       	fr = open(fileName,"rb")
+       	print("sending ",fileName)
+       	fileName = "NOF$"+fileName # NOF Name of File
+       	fs.sendmsg(fileName.encode())
+       	print("received:", fs.receivemsg())
+       	fileInServer = fs.receivemsg().decode()
+       	spl = fileInServer.split("$")
+       	if spl[0] == "FIS": # File Is in Server
+       	 print("file already in server")
+       	else:
+       	 while True:
+       	  reading = fr.read(100)
+       	  while reading:
+       	   print("sending ",(reading))
+       	   fs.sendmsg(reading)
+       	   print("received:", fs.receivemsg())
+       	   reading = fr.read(100)
+       	  fr.close()
+       	  break
 
 for i in range(100):
     ClientThread(serverHost, serverPort, debug)
--- emphaticDemo/framedThreadServer.py
+++ emphaticDemo/framedThreadServer.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 import sys, os, socket, params, time
+import threading
 from threading import Thread
 from framedSock import FramedStreamSock
 
@@ -14,6 +15,8 @@ paramMap = params.parseParams(switchesVarDefaults)
 
 debug, listenPort = paramMap['debug'], paramMap['listenPort']
 
+lock = threading.Lock() # Mutex object
+
 if paramMap['usage']:
     params.usage()
 
@@ -24,24 +27,44 @@ lsock.listen(5)
 print("listening on:", bindAddr)
 
 class ServerThread(Thread):
-    requestCount = 0            # one instance / class
+    requestCount = 0        # one instance / class
     def __init__(self, sock, debug):
+        global lock
         Thread.__init__(self, daemon=True)
         self.fsock, self.debug = FramedStreamSock(sock, debug), debug
         self.start()
     def run(self):
+        global lock
         while True:
             msg = self.fsock.receivemsg()
             if not msg:
-                if self.debug: print(self.fsock, "server thread done")
-                return
+                if True: print(self.fsock, "server thread done")
+                lock.release() # Release lock
+                return    
             requestNum = ServerThread.requestCount
             time.sleep(0.001)
             ServerThread.requestCount = requestNum + 1
+            isAFileName = msg.decode()
             msg = ("%s! (%d)" % (msg, requestNum)).encode()
             self.fsock.sendmsg(msg)
+            spl = isAFileName.split("$")
+            if spl[0] == "NOF":
+             if not os.path.isfile("serverFiles/"+spl[1]):
+              self.fsock.sendmsg(b"good to go")
+              fileName = spl[1]
+              with open("serverFiles/"+fileName,"wb") as file:
+               print("Allocating file "+fileName)
+               while True:
+               	data = self.fsock.receivemsg()
+               	if not data:
+               	 break
+               	file.write(data)
+               	self.fsock.sendmsg(data)
+             else:
+               self.fsock.sendmsg(b"FIS$")
 
 
 while True:
+    lock.acquire() # Lock activated 
     sock, addr = lsock.accept()
     ServerThread(sock, debug)
